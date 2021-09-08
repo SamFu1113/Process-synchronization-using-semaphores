5518ab1eda5561db4dd488723cf775611259347a
sam <sam86111374@gmai.com>
2021-09-08 17:04:23 +0800

    Finish the to-do work.

--- proj3/p2.c
+++ proj3/p2.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include "awk_sem.h"
 
-main() {
+int main() {
     int i = 0 ;
     // *** please insert proper semaphore initialization here
+    int semId1 = get_sem(".",'1');
+    int semId2 = get_sem(".",'2');    
     do {
         // *** this is where you should place semaphore 
-       
+       //V(semid0);
+       //V(semId0);
+       //V(semId1);
+       P(semId1);
+       //V(semId2);
        printf("P222222222 is here\n"); i++ ;
-       
+       //V(semId0);
+       V(semId2);
+       V(semId2);
        // *** this is where you should place semaphore
    
     }  while (i < 100);
